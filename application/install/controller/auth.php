6faf31cfc7f7945aee183a2273532a6d5b1cf602
够过瘾 <245629560@qq.com>
2017-11-08 10:31:13 +0800

    开启已安装提示

--- application/install/controller/auth.php
+++ application/install/controller/auth.php
@@ -10,7 +10,7 @@ class auth extends controller {
     public function __construct()
     {
 
-        if(file::exists(APP_PATH.'/install/install.lock')){
+        if(file::exists(RUNTIME_PATH.'/install.lock')){
             exit('<stong>程序已经安装，如果您确定要重新安装，请先删除 runtime/install.lock 再重新运行</stong>');
         }
 
