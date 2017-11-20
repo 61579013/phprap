3f7256a6c8a6e7b1e7dbde8cfbe8f27681b0b1c6
够过瘾 <245629560@qq.com>
2017-11-20 15:20:42 +0800

    public function __construct($message, $sql, $code = 0)

--- gophp/library/exception.php
+++ gophp/library/exception.php
@@ -12,7 +12,7 @@ class exception extends \Exception
 
     protected $sql;
 
-    public function __construct($message, $sql, $code = '')
+    public function __construct($message, $sql, $code = 0)
     {
 
         parent::__construct($message, $code);
