56ff1362d7e773bb0b7b8b2454efde018caf4cf7
gouguoyin <245629560@qq.com>
2019-09-20 11:54:56 +0800

    版本更新到2.0.4，新增项目字段模板功能

--- runtime/install/db.sql
+++ runtime/install/db.sql
@@ -289,6 +289,19 @@ CREATE TABLE `doc_template` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `doc_template`
+--
+
+INSERT INTO `doc_template` (`id`, `encode_id`, `project_id`, `header_fields`, `request_fields`, `response_fields`, `status`, `creater_id`, `updater_id`, `created_at`, `updated_at`) VALUES
+(1, '2019064870', 1, '[{\"id\":\"2\",\"parent_id\":\"0\",\"level\":\"0\",\"name\":\"Content-Type\",\"title\":\"\",\"type\":\"string\",\"required\":\"10\",\"value\":\"application\\/json\",\"remark\":\"\"}]', NULL, '[{\"id\":\"2\",\"parent_id\":\"0\",\"level\":\"0\",\"name\":\"status\",\"title\":\"请求状态\",\"type\":\"string\",\"example_value\":\"success\",\"remark\":\"success代表请求成功,error代表请求失败\"},{\"id\":\"3\",\"parent_id\":\"0\",\"level\":\"0\",\"name\":\"code\",\"title\":\"状态码\",\"type\":\"string\",\"example_value\":\"200\",\"remark\":\"200代表成功\"},{\"id\":\"4\",\"parent_id\":\"0\",\"level\":\"0\",\"name\":\"message\",\"title\":\"返回信息\",\"type\":\"string\",\"example_value\":\"\",\"remark\":\"\"}]', 10, 2, 2, '2019-09-19 23:21:27', '2019-09-20 03:42:01');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `doc_user`
+--
+
 -- --------------------------------------------------------
 
 --
@@ -435,8 +448,8 @@ ALTER TABLE `doc_project_log`
 ALTER TABLE `doc_template`
   ADD PRIMARY KEY (`id`,`encode_id`),
   ADD UNIQUE KEY `encode_id` (`encode_id`),
+  ADD UNIQUE KEY `project_id` (`project_id`) USING BTREE,
   ADD KEY `creater_id` (`creater_id`),
-  ADD KEY `project_id` (`project_id`) USING BTREE,
   ADD KEY `status` (`status`);
 
 --
